e55c61dbc0f456b5ba87785b49876ffe0384a6ac
Alexander Hoehne <info@myfoxit.com>
2020-10-07 08:53:09 +0200

    Initial commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,28 @@
+import  React,{ useState } from "react";
+
+export const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    onFormSubmit(term);
+  };
+
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+          <label>Search fo a video</label>
+          <input
+            type="text"
+            value={term}
+            onChange={(event) => setTerm(event.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+
